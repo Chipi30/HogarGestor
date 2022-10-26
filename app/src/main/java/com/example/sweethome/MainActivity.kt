b5b77e90fb54524a7c15c7612054784bdd2735c4
Karen Hernández <86068762+Chipi30@users.noreply.github.com>
2022-10-25 20:31:41 -0500

    kj

--- app/src/main/java/com/example/sweethome/MainActivity.kt
+++ app/src/main/java/com/example/sweethome/MainActivity.kt
@@ -4,35 +4,40 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     private var editUsername: EditText?= null
-    private var editPassword : TextInputEditText?=null
+    private var editPassword : EditText?=null
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         editUsername = findViewById(R.id.editUserName)
         editPassword = findViewById(R.id.editPassword)
+
         val btnRegister = findViewById<Button>(R.id.btnRegister)
         val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        fun onLogin(botonLogin: View){
+            if(editUsername!!.text.toString() == "kkk@gmail.com" && editPassword!!.text.toString() == "#Kim1297") {
+                val intent = Intent(this, WelcomeActivity::class.java)
+                startActivity(intent)
+            }else{
+                val dialog = AlertDialog.Builder(this).setTitle("ERROR!!").setMessage("@string/errorText").create().show()
+            }
+        }
 
         btnRegister?.setOnClickListener{
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
-
-        btnLogin?.setOnClickListener{
-            val intent = Intent(this, NavegationActivity::class.java)
-            startActivity(intent)
-        }
-
-
-
 
     }
 }
--- app/src/main/java/com/example/sweethome/RegisterActivity.kt
+++ app/src/main/java/com/example/sweethome/RegisterActivity.kt
@@ -37,7 +37,7 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         btnRegister?.setOnClickListener{
-            val intent = Intent(this, NavegationActivity::class.java)
+            val intent = Intent(this, WelcomeActivity ::class.java)
             startActivity(intent)
         }
     }
