e7622be770e408802d883255d60f04ae4286a739
Leonardo Martínez <leonardo.mu00@gmail.com>
2022-10-28 08:31:57 -0500

    Segundo Commit

--- app/src/main/java/com/example/sweethome/MainActivity.kt
+++ app/src/main/java/com/example/sweethome/MainActivity.kt
@@ -4,29 +4,13 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Patterns
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
-import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
     private var editUsername: EditText?= null
     private var editPassword : TextInputEditText?=null
-    private val password_Pattern: Pattern = Pattern.compile(
-        "^"+
-                "(?=.*[0-9])"+
-                "(?=.*[a-z])"+
-                "(?=.*[A-Z])"+
-                "(?=.*[^A-Za-z0-9])"+
-                "(?=.*[@#\$%+=])"+
-                "(?=\\S+$)"+
-                ".{8,}"+
-                "$"
-    )
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,59 +26,9 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-       /* btnLogin?.setOnClickListener{
+        btnLogin?.setOnClickListener{
             val intent = Intent(this, NavegationActivity::class.java)
             startActivity(intent)
-        }*/
-
-
-
-
-    }
-
-    fun onLogin(view: View) {
-
-        if(ValidarLoguin()){
-            Toast.makeText(this,"Usuario Correcto", Toast.LENGTH_LONG).show()
-
-        } else{
-            Toast.makeText(this,"Usuario o contraseña no registrado", Toast.LENGTH_LONG).show()
         }
-
-
-    }
-
-    private fun ValidarLoguin(): Boolean {
-        var validar = true
-        val emailInput = editUsername!!.text.toString()
-        val passwordInput = editPassword!!.text.toString()
-
-        if (editUsername!!.text.toString() == "sweet@home.com") {
-            if (editPassword!!.text.toString() == "Sweet22+") {
-                val intent = Intent(this, NavegationActivity::class.java)
-                startActivity(intent)
-            }
-        }else {
-            validar = false
-        }
-
-        if (TextUtils.isEmpty(editUsername!!.text.toString())) {
-            editUsername!!.error = "Campo requerido"
-            validar = false
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
-            editUsername!!.error = "Debe ser un correo valido"
-            validar = false
-        } else editUsername!!.error = null
-
-        if (TextUtils.isEmpty(editPassword!!.text.toString())) {
-            editPassword!!.error = "Campo requerido"
-            validar = false
-        } else if (!password_Pattern.matcher(passwordInput).matches()) {
-            editPassword!!.error = "Contraseña incorrecta"
-            validar = false
-        } else editPassword!!.error = null
-
-        return validar
-
     }
 }
--- app/src/main/java/com/example/sweethome/RegisterActivity.kt
+++ app/src/main/java/com/example/sweethome/RegisterActivity.kt
@@ -1,12 +1,17 @@
 package com.example.sweethome
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
+import java.util.regex.Pattern
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -16,6 +21,9 @@ class RegisterActivity : AppCompatActivity() {
     private var phone : EditText? = null
     private var email : EditText? = null
     private var editPassword : TextInputEditText?=null
+    private var confirmPassword : TextInputEditText?=null
+    private var checKPolicies : CheckBox? = null
+    private val text_pattern : Pattern = Pattern.compile("[a-zA-Z]*")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +35,8 @@ class RegisterActivity : AppCompatActivity() {
         phone = findViewById(R.id.phone)
         email = findViewById(R.id.email)
         editPassword = findViewById(R.id.editPassword)
+        confirmPassword = findViewById(R.id.confirmPassword)
+        checKPolicies = findViewById(R.id.checkPolicies)
 
         val btnRegister = findViewById<Button>(R.id.btnRegister)
         val link_to_login = findViewById<TextView>(R.id.link_to_login)
@@ -35,10 +45,76 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
+    }
 
-        btnRegister?.setOnClickListener{
-            val intent = Intent(this, WelcomeActivity ::class.java)
+    fun onRegister(view: View) {
+        if(ValidateForm()){
+            val intent = Intent(this, NavegationActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    fun onCheck(view: View) {
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+        if(checKPolicies!!.isChecked) {
+            btnRegister.isEnabled = true
+            btnRegister.isClickable = true
+            btnRegister.setTextColor(Color.BLACK)
+            btnRegister.setBackgroundColor(Color.rgb(66,142,146))
+        }
+        else{
+            btnRegister.isEnabled = false
+            btnRegister.isClickable = false
+            btnRegister.setTextColor(Color.BLACK)
+            btnRegister.setBackgroundColor(Color.GRAY)
+        }
+    }
+
+    private fun ValidateForm() : Boolean {
+        var validate: Boolean = true
+        val user = userName!!.text.toString()
+        val lastName = userLastName!!.text.toString()
+        if(TextUtils.isEmpty(user))
+        {
+            userName!!.error = "@strings/required"
+            validate = false
+        } else if (!text_pattern.matcher(user.replace(" ", "")).matches()){
+            userName!!.error = "Nombre invalido"
+            validate = false
+        }
+        if(TextUtils.isEmpty(lastName))
+        {
+            userLastName!!.error = "@strings/required"
+            validate = false
+        } else if (!text_pattern.matcher(lastName.replace(" ", "")).matches()){
+            userLastName!!.error = "Apellido invalido"
+            validate = false
+        }
+        if(TextUtils.isEmpty(nit!!.text.toString()))
+        {
+            nit!!.error = "@strings/required"
+            validate = false
+        }
+        if(TextUtils.isEmpty(phone!!.text.toString()))
+        {
+            phone!!.error = "@strings/required"
+            validate = false
+        }
+        if(TextUtils.isEmpty(email!!.text.toString()))
+        {
+            email!!.error = "@strings/required"
+            validate = false
+        }
+        if(TextUtils.isEmpty(editPassword!!.text.toString()))
+        {
+            editPassword!!.error = "@strings/required"
+            validate = false
+        }
+        if(TextUtils.isEmpty(confirmPassword!!.text.toString()))
+        {
+            confirmPassword!!.error = "@strings/required"
+            validate = false
+        }
+        return validate
     }
 }
